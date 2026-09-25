f2491408b92dda4b3b7b7741edf5ffb5e304c0a6
JoseCOCA <josecoca0890@gmail.com>
2026-09-24 20:16:22 -0600

    pendientes y notas

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+// tipos de datos primitivos 
+// variables 
 var x = 1;
 let y = 2 + 3 - 2 / 3 + x;
 let n = true;
 let e = false;
 
 // Tarea: Investigar la diferencia entre let y var en javascript
+
+// cadena de texto o string
 const f = "hola";
 
+// PENDIENTE: ver en los metodos de una string 
 f.includes('ho')
 console.log(f.length)
 
 
+// reasignacion de variable
 y = ['sja', f]
 
 // Comentatio una linea
@@ -19,18 +25,20 @@ y = ['sja', f]
  *  
  */
 
-// ARREGLOS
+
+// DEFINICION DE ARREGLOS
 var r = [1,2,3,4,5,6,7,8,9];
+
+// indices en arreglos
 r[0] // 1 
 r[1] // 2
 
 
+// PENDIENTE: ver los metodos de un arreglo
 r.includes()
 r.length
 
 // TRABAJAR CON ARREGLOS EN LA SIGUIENTE CLASE
-
-
 let objt = {
     property1 : 1,
     property2: 2.0,
